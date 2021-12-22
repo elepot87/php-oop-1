e742c5cf161ed13535c4b922f0a0b86a7d645b75
elepot87 <eleonorapotente@gmail.com>
2021-12-22 15:47:30 +0100

    setup e prova prima istanza

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+require_once __DIR__ . '/partials/movie.php'
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHO OOP</title>
+</head>
+
+<body>
+    <?php 
+var_dump($JurassicPark);
+?>
+
+</body>
+
+</html>
---
+++ partials/movie.php
@@ -0,0 +1,44 @@
+<!--  è definita una classe ‘Movie’
+   => all’interno della classe sono dichiarate delle variabili d’istanza
+   => all’interno della classe è definito un costruttore
+   => all’interno della classe è definito almeno un metodo
+- vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà -->
+
+<?php
+
+class Movie {
+// Variabili
+public $title;
+public $year;
+public $news;
+public $description;
+public $genre;
+
+// Costruttore
+function __construct($year) {
+    $this -> year = $year;
+}
+
+// Metodo
+public function setNews($year) {
+if($year > 2020) {
+   return $this -> news = "Novità";
+} else {
+   return $this -> news = "Classico";
+}
+}
+
+public function getNews() {
+return $this -> news;
+}
+
+}
+
+
+$JurassicPark = new Movie(1993);
+$JurassicPark -> title = 'Jurassic Park';
+$JurassicPark -> setNews($year);
+$news_JurassicPark = $JurassicPark -> getNews();
+$JurassicPark -> description = 'Su Isla Nublar, una piccola isola di proprietà della InGen situata a 120 miglia a nord-ovest della Costa Rica, un operaio viene assalito e ucciso da una feroce creatura, custodita in una gabbia che la vittima stava spostando insieme ai colleghi. La famiglia dell\'uomo fa causa al proprietario della InGen, John Hammond, un simpatico ed eccentrico miliardario con la passione per i dinosauri.';
+$JurassicPark -> genre = 'Azione';
+?>
